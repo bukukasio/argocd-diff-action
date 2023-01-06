bd97ccceddfaa7a84531abc8c86ef7ae02fe99cb
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2023-01-06 16:46:14 +0530

    add step logurl comment
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -1780,6 +1780,15 @@ function getApps() {
         catch (e) {
             if (e instanceof http_errors_1.HttpError && e.message === 'Body is too long (maximum is 65536 characters)') {
                 core.error('Error: Body of HTTP request is too long.');
+                // comment on pull request with step logs details
+                const logURL = "https://github.com/" + github.context.repo.owner + "/" + github.context.repo.repo + "/actions/runs/" + github.context.runId;
+                const { owner, repo } = github.context.repo;
+                yield octokit.rest.issues.createComment({
+                    issue_number: github.context.issue.number,
+                    owner,
+                    repo,
+                    body: `Error: Body of HTTP request is too long. Please check the logs for more details here: ${logURL}`
+                });
                 process.exit(1);
             }
             throw e;
